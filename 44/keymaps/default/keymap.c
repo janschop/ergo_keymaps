6c1b047aa05018a282f1afdf04258bfe80bd8f87
janschop <jan.erik@schopmeier.com>
2023-08-05 22:22:20 +0200

    update before change to other layer setup

--- 44/keymaps/default/keymap.c
+++ 44/keymaps/default/keymap.c
@@ -22,11 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ortho_split_3x6_4(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, KC_LEFT_BRACKET,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,KC_LEFT_BRACKET,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, KC_LGUI ,KC_LALT, KC_BSPC,   TT(1),                        TT(2),  KC_SPC, KC_RALT, KC_ENT,XXXXXXX,XXXXXXX
                       //`---------------------------------'                     `----------------------------------'
@@ -35,23 +35,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_ortho_split_3x6_4(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,     KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+      KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LSFT,KC_ESCAPE, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                       KC_EQL,  XXXXXXX,  XXXXXXX,XXXXXXX, KC_RBRC, KC_BSLS,
+     KC_LCTL,KC_ESCAPE, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                       KC_EQL, XXXXXXX, XXXXXXX, XXXXXXX, KC_RBRC, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_NUBS, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                      XXXXXXX,  XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX,  KC_ENT,
+      KC_LSFT, KC_NUBS, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                      XXXXXXX, XXXXXXX, _______, _______, _______, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, KC_LGUI, KC_LALT,KC_DELETE,_______,                        TT(3),  KC_SPC, KC_RALT, KC_ENT,XXXXXXX,XXXXXXX
                       //`---------------------------------'                     `----------------------------------'
-  ),
+  )
 
     [2] = LAYOUT_ortho_split_3x6_4(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_GRV,  KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LSFT,KC_ESCAPE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_PLUS, KC_HOME,   KC_UP,  KC_END, KC_RCBR, KC_PIPE,
+     KC_LCTL,KC_ESCAPE, XXXXXXX, XXXXXXX,   KC_F4, XXXXXXX,                      KC_PLUS, KC_HOME,   KC_UP,  KC_END, KC_RCBR, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_DOWN,KC_RIGHT, XXXXXXX,  KC_ENT,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_DOWN,KC_RIGHT, XXXXXXX,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------| 
       XXXXXXX, XXXXXXX, KC_LGUI, KC_LALT,KC_DELETE,  TT(4),                      _______,  KC_SPC, KC_RALT,  KC_ENT, XXXXXXX, XXXXXXX
                       //`---------------------------------'                     `----------------------------------'
@@ -71,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
       [4] = LAYOUT_ortho_split_3x6_4(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_P7,   KC_P8,   KC_P9, KC_PSLS, KC_PAST,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_NUM,   KC_P7,   KC_P8,   KC_P9, KC_PSLS, KC_PAST,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_P4,   KC_P5,   KC_P6, KC_PMNS, KC_PPLS,
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_TAB,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PMNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_P1,   KC_P2,   KC_P3, XXXXXXX, KC_PENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -82,7 +82,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+
 uint16_t init_done = 0;
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+
+//     case KC_U:
+//         // Detect the activation of only Left Alt
+//         if (get_mods() == MOD_BIT(KC_RALT)) {
+//             if (record->event.pressed) {
+//                 // No need to register KC_LALT because it's already active.
+//                 // The Alt modifier will apply on this KC_TAB.
+//                 register_code(LSFT(KC_7));
+//             } else {
+//                 unregister_code(LSFT(KC_7));
+//             }
+//             // Do not let QMK process the keycode further
+//             return false;
+//         }
+//         // Else, let QMK process the KC_ESC keycode as usual
+//         return true;
+
+//     }
+//     return true;
+// }
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     switch (keycode) {
+
+//     case KC_U:
+//         // Detect the activation of only Left Alt
+//         if (get_mods() == MOD_BIT(KC_RALT)) {
+//             if (record->event.pressed) {
+//                 // No need to register KC_LALT because it's already active.
+//                 // The Alt modifier will apply on this KC_TAB.
+//                 unregister_code(LSFT(KC_7));
+//             } else {
+//                 unregister_code(LSFT(KC_7));
+//             }
+//             // Do not let QMK process the keycode further
+//             return false;
+//         }
+//         // Else, let QMK process the KC_ESC keycode as usual
+//         return true;
+
+//     }
+//     return true;
+// };
 
 // layer_state_t layer_state_set_user(layer_state_t state) {
 //   if (init_done == 0) {
