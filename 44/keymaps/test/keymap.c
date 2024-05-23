f6d6b346e11f7b12c76deab6cc5bb59112c09c32
janschop <jan.erik@schopmeier.com>
2024-05-23 20:04:51 +0200

    commmit

--- 44/keymaps/test/keymap.c
+++ 44/keymaps/test/keymap.c
@@ -306,23 +306,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
         [3] = LAYOUT_ortho_split_3x6_4(//function and algr
-    _______,  KC_GRV, ALGR(KC_2), ALGR(KC_3), S(KC_4), ALGR(KC_5),    _______,    _______,    S(KC_8),    S(KC_9),       _______,       _______, 
-    _______,   KC_F1,      KC_F2,      KC_F3,   KC_F4,      KC_F5,    KC_F6  , ALGR(KC_7), ALGR(KC_8), ALGR(KC_9),    ALGR(KC_0), ALGR(KC_RBRC),        
-    TO(0)  ,   KC_F7,      KC_F8,      KC_F9,  KC_F10,     KC_F11,    KC_F12,        mail,    S(KC_8),    S(KC_9), ALGR(KC_RBRC),         TO(0),                    
-    XXXXXXX, XXXXXXX,    _______,    _______, _______,    _______,    XXXXXXX,    XXXXXXX,    _______,    _______,       _______,       _______                     
+    _______,  KC_GRV, ALGR(KC_2), ALGR(KC_3), S(KC_4), ALGR(KC_5),    _______,    _______,    S(KC_8),    S(KC_9),       _______, _______, 
+    _______,   KC_F1,      KC_F2,      KC_F3,   KC_F4,      KC_F5,    KC_F6  , ALGR(KC_7), ALGR(KC_8), ALGR(KC_9),    ALGR(KC_0), _______,        
+    TO(0)  ,   KC_F7,      KC_F8,      KC_F9,  KC_F10,     KC_F11,    KC_F12,        mail,    S(KC_8),    S(KC_9), ALGR(KC_RBRC),   TO(0),                    
+    XXXXXXX, XXXXXXX,    _______,    _______, _______,    _______,    XXXXXXX,    XXXXXXX,    _______,    _______,       _______, _______                     
     ),
 
     [5] = LAYOUT_ortho_split_3x6_4(// mouse
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
-    TO(0)  , KC_BTN4, KC_BTN1, KC_WH_U, KC_BTN2, KC_BTN5,    LGUI(KC_D), lft_dsktp, KC_MS_U, rght_dsktp, XXXXXXX,   TO(0),    
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  lft_dsktp, G(KC_TAB), rght_dsktp, XXXXXXX, XXXXXXX,
+    TO(0)  , KC_BTN4, KC_BTN1, KC_WH_U, KC_BTN2, KC_BTN5,    XXXXXXX, LGUI(KC_D),   KC_MS_U,    XXXXXXX, XXXXXXX,   TO(0),    
     TO(0)  , KC_BTN3, KC_WH_L, KC_WH_D, KC_WH_R,   TO(0),    XXXXXXX,    KC_MS_L,   KC_MS_D,    KC_MS_R, XXXXXXX,   TO(0),             
-    XXXXXXX, XXXXXXX,   TO(0),   TO(0),   TO(0),   TO(0),    TO(0),      TO(0),     TO(0),        TO(0), XXXXXXX, XXXXXXX                 
+    XXXXXXX, XXXXXXX,   TO(0),   TO(0),   TO(0),   TO(0),    TO(0)  ,      TO(0),     TO(0),      TO(0), XXXXXXX, XXXXXXX                 
     ),
 
     [4] = LAYOUT_ortho_split_3x6_4(//numpad
-    QK_BOOT, QK_BOOT,    kiwi,bunnpris,     rema, XXXXXXX,    KC_NUM,  KC_P7,   KC_P8,   KC_P9, KC_PSLS, KC_PAST,
-    XXXXXXX, XXXXXXX, XXXXXXX,   KC_UP,  XXXXXXX, XXXXXXX,    KC_TAB,  KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PMNS,
-    XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT,   TO(0),    XXXXXXX, KC_P1,   KC_P2,   KC_P3, KC_PENT,   TO(0),
-    XXXXXXX, XXXXXXX,  KC_ENT, KC_LGUI,  KC_BSPC, _______,    _______, KC_P0, KC_PDOT, _______, XXXXXXX, XXXXXXX
+    QK_BOOT, QK_BOOT,    kiwi, bunnpris,     rema, XXXXXXX,    KC_NUM,  KC_P7,   KC_P8,   KC_P9, KC_PSLS, KC_PAST,
+    XXXXXXX, XXXXXXX, XXXXXXX,    KC_UP,  XXXXXXX, XXXXXXX,    KC_TAB,  KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PMNS,
+    XXXXXXX, XXXXXXX, KC_LEFT,  KC_DOWN, KC_RIGHT,   TO(0),    XXXXXXX, KC_P1,   KC_P2,   KC_P3, KC_PENT,   TO(0),
+    XXXXXXX, XXXXXXX,  KC_ENT,  KC_LGUI,  KC_BSPC, _______,    _______, KC_P0, KC_PDOT, _______, XXXXXXX, XXXXXXX
     ),
 };
